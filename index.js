e8ad82e42aeb6dd54bbf4e303523927c51dae3c8
niki-sal <ne.salehi@gmail.com>
2021-02-04 12:15:39 -0800

    SWAPI set up

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const axios = require('axios')
 const expressLayouts = require('express-ejs-layouts')
 const methodOverride = require('method-override')
+
 
 //Middleware
 app.use(expressLayouts)
@@ -21,6 +23,45 @@ app.get("/",(req, res) =>{
     res.send('hello')
 })
 
+// app.get('/omdb',(req, res)=>{
+//     const qs = {
+//         params:{
+//             s: 'star wars',
+//             apikey: process.env.API_KEY
+//         }
+//     }
+//     //http://www.omdbapi.com/?s=star+wars&apikey=OUR+key
+//     axios.get('http://www.omdbapi.com', qs)
+//     .then((response) =>{
+//         console.log(response.data)
+
+//     })
+// })
+
+//with the swapi api
+app.get('/swapi/search', (req, res)=>{
+    res.render('search')
+
+})
+app.get('/swapi/show', (req, res)=>{
+    console.log('made it here')
+    
+    console.log('query',req.query)
+    // basic api call to get a person
+    axios.get(`https://swapi.dev/api/people/${req.query.personId}`)
+    .then((response) =>{
+        //response.data is where our data lives
+        console.log(response.data)
+        //make a person object
+        const person = {
+            name: response.data.name,
+            birth: response.data['birth_year'],
+            home: response.data.homeworld
+        }
+        res.render('show',person)
+    })
+  
+})
 //Listen
 const PORT = process.env.PORT || 8000
 app.listen(PORT, console.log(`Listening on  ${PORT}`))
