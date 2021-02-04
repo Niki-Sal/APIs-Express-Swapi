ade1087fcb582ed52a8e91089db070bc16e510f6
niki-sal <ne.salehi@gmail.com>
2021-02-04 11:04:12 -0800

    server up and running

--- index.js
+++ index.js
@@ -2,5 +2,25 @@
 require('dotenv').config()
 
 //see variables using process.env
-console.log(process.env)
-console.log(process.env.API_KEY)
+// console.log(process.env)
+// console.log(process.env.API_KEY)
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const methodOverride = require('method-override')
+
+//Middleware
+app.use(expressLayouts)
+app.set('view engine' , 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+
+//Routes
+app.get("/",(req, res) =>{
+    res.send('hello')
+})
+
+//Listen
+const PORT = process.env.PORT || 8000
+app.listen(PORT, console.log(`Listening on  ${PORT}`))
